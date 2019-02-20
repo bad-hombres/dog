cec41fb58e98b6785c2eec23affada992423e32a
xirax <mwest67@gmail.com>
2019-02-20 15:18:21 +0000

    Setup for pyipi

--- dogclient/setup.py
+++ dogclient/setup.py
@@ -34,21 +34,7 @@ setup(
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: BountyHunters',
-        'Topic :: Bounty Hunting :: Automation',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7'
     ],
 
